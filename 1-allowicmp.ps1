3e1f3bb6a3a20010f511a1d0e710c44f2fbfebb5
rfh-12345 <60385550+rfh-12345@users.noreply.github.com>
2020-01-28 10:14:39 +0000

    Add files via upload

---
+++ 1-allowicmp.ps1
@@ -0,0 +1,11 @@
+﻿New-NetFirewallRule `
+-Name 'ICMPv4' `
+-DisplayName 'ICMPv4' `
+-Description 'Allow ICMPv4' `
+-Profile Any `
+-Direction Inbound `
+-Action Allow `
+-Protocol ICMPv4 `
+-Program Any `
+-LocalAddress Any `
+-RemoteAddress Any
